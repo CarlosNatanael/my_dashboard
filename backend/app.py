0c022271f876c302db4a87f22798f63254bd45ce
Srleo12 <carlosnatha2345@gmail.com>
2026-01-27 10:01:03 -0300

    test: criado estrutura motor app.py

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from models import db, Claim
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ra_dashboard.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    claims = Claim.query.order_by(Claim.update_at.desc()).all()
+    return render_template('index.html', claims=claims)
+
+if __name__ == "__main__":
+    app.run(debug=True)
